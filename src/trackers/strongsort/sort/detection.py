88011028c33f926bd141ba85aa1ab4da55f9bc20
Scarpelli <112524689+MaSNogueiraS@users.noreply.github.com>
2023-04-20 15:48:05 -0300

    Update detection.py
    Fiz uma alteração na linha 32 adicionando .detach() que corrige um erro de numpy que não permitia que o código rodasse

--- src/trackers/strongsort/sort/detection.py
+++ src/trackers/strongsort/sort/detection.py
@@ -29,7 +29,7 @@ class Detection(object):
     def __init__(self, tlwh, confidence, feature):
         self.tlwh = np.asarray(tlwh, dtype=np.float32)
         self.confidence = float(confidence)
-        self.feature = np.asarray(feature.cpu(), dtype=np.float32)
+        self.feature = np.asarray(feature.cpu().detach(), dtype=np.float32)
 
     def to_tlbr(self):
         """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
