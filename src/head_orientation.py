bbc0b583ff0179c6c1cdbeffedc8192f0c99886c
MaSNogueiraS <mateus.scarpelli03@gmail.com>
2024-07-02 14:39:47 -0300

    add some comments

--- src/head_orientation.py
+++ src/head_orientation.py
@@ -10,8 +10,14 @@ from std_msgs.msg import Int32, Bool
 
 class HeadOrientation:
     def __init__(self) -> None:
+
+        # Services:
+
         rospy.wait_for_service('/joint_command')
         self.manipulator = rospy.ServiceProxy('/joint_command', Joint_service)
+
+        # Messages:
+
         rospy.wait_for_message('/tracker/risk_and_direction', riskanddirection)
         self.risk_direction = rospy.Subscriber('/tracker/risk_and_direction', riskanddirection, self.callback)
         rospy.wait_for_message('/tracker/id_detected', Bool)
@@ -21,18 +27,20 @@ class HeadOrientation:
         rospy.wait_for_message('/recovery_status', Bool)
         self.recovery_status = rospy.Subscriber('/recovery_status', Bool, self.callbackRecoveryStatus)
 
+        # Variables:
+
         self.direction = 0.0
         self.MOTOR_POSITION = 0.0
         self.joint_goal = Joint_Goal()
         self.joint_goal.id = 10
         self.joint_goal.x = self.MOTOR_POSITION
-
         self.assist_goal = Joint_Goal()
         self.assist_goal.id = 9
         self.assist_goal.x = 0.0
         rospy.loginfo('Ready to Orientate Head!')
 
     def callbackPersonDetected(self, data):
+        """Callback function for the subscriber of the topic /tracker/id_detected"""
         try:
             if data == None:
                 self.id_detected = False
@@ -42,6 +50,7 @@ class HeadOrientation:
             print(traceback.format_exc())
 
     def callbackRecoveryStatus(self, data):
+        """Callback function for the subscriber of the topic /recovery_status"""
         try:
             if data == None:
                 self.recovery_status = False
@@ -51,6 +60,7 @@ class HeadOrientation:
             print(traceback.format_exc())
 
     def callbackStartOrientation(self, data):
+        """Callback function for the subscriber of the topic /startOrientation"""
         try:
             if data == None:
                 self.start_orientation = False
@@ -60,6 +70,7 @@ class HeadOrientation:
             print(traceback.format_exc())
 
     def callback(self, data):
+        """Callback function for the subscriber of the topic /tracker/risk_and_direction"""
         try:
             if data == None:
                 self.direction = self.direction
@@ -69,6 +80,7 @@ class HeadOrientation:
             print(traceback.format_exc())
 
     def move_head(self, direction):
+        """Function to move the head of the robot based on the direction of the person"""
         if self.id_detected == True:
             c_direction = int(direction.data)
             if 1 < c_direction < 6 and self.MOTOR_POSITION >= -1.5:
@@ -91,6 +103,7 @@ class HeadOrientation:
 
 
     def main(self):
+        """Main function to run the node"""
         while not rospy.is_shutdown():
             if self.start_orientation == True and (self.recovery_status == False or self.recovery_status == None):
                 self.move_head(self.direction)
